ea6e794ef47a150bbf78cd147606884351435e95
shadex7 <ishadex777@gmail.com>
2023-09-04 23:48:16 +0300

    init

--- src/metadata/documents/index.js
+++ src/metadata/documents/index.js
@@ -2,7 +2,11 @@
 
 // движение денег
 //import doc_issue from "./doc_issue";
+import lines_availability from '../../../wb-paperless/server/meta';
+import FrmObj from '../../components/LinesAvailability/FrmObj'
 
 export default function ($p) {
+  lines_availability($p);
+  $p.doc.lines_availability.FrmObj = FrmObj;
   //doc_issue($p);
 }
